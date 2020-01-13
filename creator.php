f06c7183c37dfe16f99035af1a3da2d91a516493
Middi <richard@richardmiddleton.me>
2020-01-13 13:32:24 +0100

    using ob_start to return MCE after str_replace

--- creator.php
+++ creator.php
@@ -3,5 +3,16 @@
 function r_recipe_creator_shortcode() {
 
     $creatorHTML = file_get_contents('creator-template.php', true);
+    $editorHTML = r_generate_content_editor();
+    $creatorHTML = str_replace('CONTENT_EDITOR', $editorHTML, $creatorHTML);
+    
     return $creatorHTML;
+}
+
+function r_generate_content_editor() {
+    ob_start();
+    wp_editor('','recipecontenteditor');
+    $editor_contents= ob_get_clean();
+
+    return $editor_contents;
 }
