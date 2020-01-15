805e7a15efdd1d47151862dd33b35d857a9ba1ca
Middi <richard@richardmiddleton.me>
2020-01-15 12:35:47 +0100

    completed shortcode

--- twitter-follow.php
+++ twitter-follow.php
@@ -1,10 +1,13 @@
 <?php
 
-function r_twitter_follow_shortcode() {
+function r_twitter_follow_shortcode($atts, $content = null) {
 
+	$atts = shortcode_atts([
+		'handle' => 'udemy',
+	], $atts);
 
-    return '<a href="https:/twitter.com/" 
+    return '<a href="https:/twitter.com/' . $atts['handle'] . '" 
 				class="button button-rounded button-aqua" target="_blank">
-				<i class="icon-twitter"></i></a>';
+				<i class="icon-twitter"></i>' . do_shortcode($content) . '</a>';
 
 }
