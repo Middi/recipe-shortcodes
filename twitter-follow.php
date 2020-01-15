2661c7b1f5097e68dbe78b236235b1c3922eb8f1
Middi <richard@richardmiddleton.me>
2020-01-15 12:25:05 +0100

    added twitter shortcode

---
+++ twitter-follow.php
@@ -0,0 +1,10 @@
+<?php
+
+function r_twitter_follow_shortcode() {
+
+
+    return '<a href="https:/twitter.com/" 
+				class="button button-rounded button-aqua" target="_blank">
+				<i class="icon-twitter"></i></a>';
+
+}
